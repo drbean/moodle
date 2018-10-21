a4a627fa7a820a2deb4448ddf113beb82a39f3d5
Dr Bean <drbean@sac.nuu.edu.tw>
2018-10-21 14:00:24 +0800

    format separator.s/<br>/&nbsp/.Have_formats_one_line,avoid_excessive_scrolling.

--- question/import_form.php
+++ question/import_form.php
@@ -58,7 +58,7 @@ class question_import_form extends moodleform {
             if (get_string_manager()->string_exists('pluginname_help', 'qformat_' . $shortname)) {
                 $separator .= $OUTPUT->help_icon('pluginname', 'qformat_' . $shortname);
             }
-            $separator .= '<br>';
+            $separator .= '&nbsp';
             $separators[] = $separator;
         }
 
