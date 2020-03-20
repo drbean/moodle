754aa300adfef18f89c3d21e34d3edba049a1224
Your Name <you@example.com>
2020-03-20 21:45:19 +0800

    s/correspondence_3/toeic_speaking_4/

--- config.php
+++ config.php
@@ -25,7 +25,7 @@ $CFG->directorypermissions = 02777;
 
 // $CFG->hiddenuserfields = array ("First name", "Surname", "Last downloaded from this course");
 
-$CFG->tag_component = 'quiz_tag_correspondence_3';
+$CFG->tag_component = 'quiz_tag_toeic_speaking_4';
 
 require_once(__DIR__ . '/lib/setup.php');
 
